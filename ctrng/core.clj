6e5470c7f1f7df5b9da09af79ec4a69a057c7d2f
mihhailko <mihhail@kattenpad.nl>
2020-04-24 00:15:27 +0200

    bugfix

--- ctrng/core.clj
+++ ctrng/core.clj
@@ -1,12 +1,15 @@
-(ns ctrng.core)
+(ns ctrng.core
+  (:require
+   [clojure.math.combinatorics]
+   [korma.core :as korma]
+   [cheshire.core :as json]
+   ))
 
-(load-file "math/combinatorics.cljc")
-
-(defn reset-board-map [m]
+(defn- reset-board-map [m]
   (let [keys (flatten (map (fn [x] (map #(str %1 %2) (map str (seq "abcdefgh")) (repeat 8 x))) (range 1 9)))]
     (into m (map vector keys (repeat 64 nil)))))
 
-(defn piece [p colour]
+(defn- piece [p colour]
   (first
    (filter p
            (filter colour
@@ -15,7 +18,7 @@
                                             {n true
                                              % true}) [:white :black]))))))
 
-(defn init-in-map [p colour m]
+(defn- init-in-map [p colour m]
   (let [rows {:white "1"
               :black "8"}
         indexes {:rook ["a" "h"]
@@ -27,12 +30,12 @@
                         (str xs (rows c))))]
     (apply merge (map #(assoc m % (piece p colour)) (fun p colour)))))
 
-(defn init-map [lst m]
+(defn- init-map [lst m]
   (if (empty? lst)
     m
     (recur (nnext lst) (merge m (init-in-map (first lst) (second lst) {})))))
 
-(defn init-pawns [p colour m]
+(defn- init-pawns [p colour m]
   (let [rows {:white "2"
               :black "7"}
         indexes {:pawn (map str (seq "abcdefgh"))}
@@ -40,35 +43,56 @@
                         (str xs (rows c))))]
     (apply merge (map #(assoc m % (piece p colour)) (fun p colour)))))
 
-(defn init-board []
+(defn- init-board []
   (merge (init-map
           (interleave (flatten (repeat 2 [:rook :knight :bishop :king :queen])) (flatten (repeat 10 [:black :white])))
           (reset-board-map {}))
          (apply merge (map #(init-pawns :pawn % {}) [:black :white]))))
 
-(def chessboard (atom {}))
+(def boardsdb (korma.db/sqlite3 {:db "../dbs/boards"}))
+
+(korma.db/defdb boards boardsdb)
+
+(korma/defentity boardgames)
+
+(defn select-chessboard [game]
+  "Returns JSON. Does not convert JSON to Clojure maps"
+  (first (korma/select boardgames (korma/where (= :gid game)))))
+
+(defn- insert-chessmap [game m]
+  (korma/insert boardgames (korma/values {:gid game
+                                          :gmap (json/generate-string m)})))
+
+(defn- update-chessboard [game m]
+  (korma/update boardgames (korma/where (= :gid game)) (korma/set-fields {:gmap (json/generate-string m)})))
+
+(defn- chessmap [game]
+  (into {}
+        (for [a (json/parse-string (:gmap (select-chessboard game)))]
+          [(first a) (clojure.walk/keywordize-keys (second a))])))
 
 (defn start-game! []
   (let [id (str (java.util.UUID/randomUUID))]
-    (swap! chessboard assoc id (init-board))
-    (println id)))
+    (do
+      (insert-chessmap id (init-board))
+      (select-chessboard id))))
 
-(defn adversary [player]
+(defn- adversary [player]
   ({:white :black :black :white} player))
 
-(defn own? [game coord player]
+(defn- own? [game coord player]
   (try
-    ((((deref chessboard) game) coord) player)
+    ((game coord) player)
     (catch Exception e nil)))
 
-(defn enemy? [game coord player]
+(defn- enemy? [game coord player]
   (try
-    ((((deref chessboard) game) coord) (adversary player))
+    ((game coord) (adversary player))
     (catch Exception e nil)))
 
-(defn what [game coord]
+(defn- what [game coord]
   (try
-    (((deref chessboard) game) coord)
+    (game coord)
     (catch Exception e nil)))
 
 (defn- moves-args [piece direct]
@@ -94,12 +118,11 @@
                  :rook {:rook (fn [n] (= n (xy piece)))}}]
     (remove ((removes piece) direct) (distinct (partition 2 (flatten (pairs direct)))))))
 
-(defn own-king [game player]
-  (let [board ((deref chessboard) game)
-        own-pieces (filter #(own? game % player) (keys board))]
+(defn- own-king [game player]
+  (let [own-pieces (filter #(own? game % player) (keys game))]
     (first (filter #(= (what game %) {:king true player true}) own-pieces))))
 
-(defn- maybe-potential-moves [game player src]
+(defn- maybe-potential-moves [mode game player src]
   (let [alpha-to-int (into {} (map vector (seq "abcdefgh") (range 1 9)))
         int-to-alpha (clojure.set/map-invert alpha-to-int)
         piece (what game src)
@@ -153,101 +176,113 @@
                               [x-right-diag]
                               (and (not (or (own? game x player) (enemy? game x player)))
                                    (and (enemy? game x-left-diag player) (enemy? game x-right-diag player)))
-                              [x x-left-diag x-right-diag]
+                              (if (= mode :normal) [x x-left-diag x-right-diag] [x-left-diag x-right-diag])
                               (and (not (or (own? game x player) (enemy? game x player)))
                                    (enemy? game x-left-diag player))
-                              [x x-left-diag]
+                              (if (= mode :normal) [x x-left-diag] [x-left-diag])
                               (and (not (or (own? game x player) (enemy? game x player)))
                                    (enemy? game x-right-diag player))
-                              [x x-right-diag]
+                              (if (= mode :normal) [x x-right-diag] [x-right-diag])
                               :else
-                              [x]))
+                              (if (= mode :normal) [x] [])))
         :else nil)
       nil)))
 
-(defn enemy-potential [game player]
-  (let [board ((deref chessboard) game)
-        enemy-pieces (filter #(own? game % (adversary player)) (keys board))
-        enemy-potential (distinct (flatten (map #(maybe-potential-moves game (adversary player) %) enemy-pieces)))]
+(defn- enemy-potential [game player]
+  (let
+      [enemy-pieces (filter #(own? game % (adversary player)) (keys game))
+       enemy-potential (distinct (flatten (map #(maybe-potential-moves :attack game (adversary player) %) enemy-pieces)))]
     (apply vector enemy-potential)))
 
-(defn potential-moves [game player src]
+(defn- potential-moves [game player src]
   (if (= src (own-king game player))
-    (apply vector (clojure.set/difference (maybe-potential-moves game player src)
-                                          (enemy-potential game player)))
-    (apply vector (maybe-potential-moves game player src))))
+    (apply vector (clojure.set/difference (set (maybe-potential-moves :normal game player src))
+                                          (set (enemy-potential game player))))
+    (apply vector (maybe-potential-moves :normal game player src))))
 
-(defn check? [game player]
-  (let [board ((deref chessboard) game)
-        own-pieces (filter #(own? game % player) (keys board))
+(defn- check? [game player]
+  (let [own-pieces (filter #(own? game % player) (keys game))
         own-potential (distinct (flatten (map #(potential-moves game player %) own-pieces)))
         chck? (some #(= (what game %) {:king true
                                        (adversary player) true}) own-potential) ]
     chck?))
 
-(defn under-check? [game player]
-  (check? game (adversary player)))
+(defn- check-e? [game player]
+  (let [own-pieces (filter #(own? game % player) (keys game))
+        enemy-pot (enemy-potential game player)
+        chck? (some #(= (what game %) {:king true
+                                       player true}) enemy-pot) ]
+    chck?))
 
-(defn test-move! [game player dest]
+(defn- under-check? [game player]
+  ;;(check? game (adversary player))
+  (check-e? game player))
+
+(defn- test-move [game player dest]
   (let [src (own-king game player)
         pot-moves (potential-moves game player src)]
     (if (some #(= % dest) pot-moves)
-      (do
-        (swap! chessboard assoc (str "test-" game) ((deref chessboard) game))
-        (swap! chessboard assoc-in [(str "test-" game) dest] (what game src))
-        (swap! chessboard assoc-in [(str "test-" game) src] nil)
-        (if (under-check? (str "test-" game) player)
+      (let [pre-testmap (assoc game dest (what game src))
+            testmap (assoc pre-testmap src nil)]
+        (if (under-check? testmap player)
           nil
           true))
       nil)))
 
-(defn run-test-moves! [game player pot-moves acc]
-  (if (empty? pot-moves)
-    acc
-    (recur game player (next pot-moves) (conj acc (test-move! game player (first pot-moves))))))
-  
-(defn checkmate? [game player]
-  (or (and (under-check? game player) (empty? (potential-moves game player (own-king game player))))
-      (and (under-check? game player) (every? nil? (run-test-moves! game player (potential-moves game player (own-king game player)) [])))))
-
-(defn post-move [game player m]
-  (cond (checkmate? game (adversary player)) (assoc m :checkmate true)
-        (check? game player) (assoc m :check true)
-        :else m))
-
-(defn move-piece! [game player src dest]
-  (let [piece-to-move (what game src)
-        pot-moves (potential-moves game player src)]
+(defn- test-next-move [game player src dest]
+  (let [pot-moves (potential-moves game player src)]
     (if (some #(= % dest) pot-moves)
-      (do
-        (swap! chessboard assoc (str "test-" game) ((deref chessboard) game))
-        (swap! chessboard assoc-in [(str "test-" game) dest] piece-to-move)
-        (swap! chessboard assoc-in [(str "test-" game) src] nil)
-        (if (under-check? (str "test-" game) player)
+      (let [pre-testmap (assoc game dest (what game src))
+            testmap (assoc pre-testmap src nil)]
+        (if (under-check? testmap player)
           nil
-          (do (swap! chessboard assoc-in [game dest] piece-to-move)
-              (swap! chessboard assoc-in [game src] nil)
-              (post-move game player {:chessboard ((deref chessboard) game)}))))
+          true))
       nil)))
 
-;;(start-game!)
+(defn- run-test-moves [game player pot-moves acc]
+  (if (empty? pot-moves)
+    acc
+    (recur game player (next pot-moves) (conj acc (test-move game player (first pot-moves))))))
+  
+(defn- checkmate? [game player]
+  (or (and (under-check? game player) (empty? (potential-moves game player (own-king game player))))
+      (and (under-check? game player) (every? nil? (run-test-moves game player (potential-moves game player (own-king game player)) [])))))
 
-;;(enemy-potential "9f10c53d-dde9-4fc9-90fd-e57a8c2957cb" :white)
+(defn- pawn? [piece]
+  (piece :pawn))
 
-;;(own-king "de9efe42-19cf-4112-ab02-fdde57c89d37" :black)
+(defn- promotions [game player]
+  (let [own-pieces (filter #(own? game % player) (keys game))
+        last-row (map #(str %1 %2) (map str (seq "abcdefgh")) (repeat 8 (if (= :white player) 8 1)))]
+    (apply vector (filter pawn? (map #(game %) (filter (set last-row) own-pieces))))))
 
-;;(potential-moves "9f10c53d-dde9-4fc9-90fd-e57a8c2957cb" :black "d8")
+(defn- promotion? [game player] (seq (promotions game player)))
 
-;;(potential-moves "de9efe42-19cf-4112-ab02-fdde57c89d37" :white "g4")
+(defn chosen [chs clr]
+  (let [opts {"rook" {:rook true (keyword clr) true}
+              "queen" {:queen true (keyword clr) true}
+              "knight" {:knight true (keyword clr) true}
+              "bishop" {:bishop true (keyword clr) true}}]
+    (opts chs)))
 
-;;(move-piece! "9f10c53d-dde9-4fc9-90fd-e57a8c2957cb" :black "c6" "c5")
+(defn promote! [game player dest promoted]
+  (let [oldboard (chessmap game)
+        midboard (dissoc oldboard "promotion")
+        newboard (assoc midboard dest promoted)]
+    (update-chessboard game newboard)))
 
-;;(checkmate? "de9efe42-19cf-4112-ab02-fdde57c89d37" :black)
+(defn- post-move [game player]
+  (cond  (check? game player) (if (checkmate? game (adversary player))
+                                (assoc game :checkmate true)
+                                (assoc game :check true))
+         (promotion? game player) (assoc game :promotion (promotions game player))
+         :else game))
 
-;;(what "de9efe42-19cf-4112-ab02-fdde57c89d37" "e4")
-
-;;(under-check? "de9efe42-19cf-4112-ab02-fdde57c89d37" :black)
-
-
-
-        
+(defn move-piece! [game player src dest]
+  (let [m (dissoc (chessmap game) "check")
+        piece-to-move (what m src)]
+    (if (test-next-move m player src dest)
+      (let [pre-newmap (assoc m dest piece-to-move)
+            newmap (assoc pre-newmap src nil)]
+        (update-chessboard game (post-move newmap player)))
+      nil)))
